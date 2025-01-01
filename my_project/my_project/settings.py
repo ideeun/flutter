ab250313630219cda8409cafbe2169b403f3fcbb
ideeun <ideeun@icloud.com>
2025-01-02 03:35:03 +0600

    setting

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -146,3 +146,14 @@ LOGGING = {
         },
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yourmailserver.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@example.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
+DEFAULT_FROM_EMAIL = 'your-email@example.com'
+
+# Не забудьте указать FRONTEND_URL для формирования правильных ссылок
+FRONTEND_URL = 'http://yourfrontend.com'
